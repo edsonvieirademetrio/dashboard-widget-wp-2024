552f40d70c9b439b85b91dc3efe550048785fd9f
edsonvieirademetrio <edsonvieirademetrio@gmail.com>
2024-06-02 22:37:03 -0300

    initial plugin file

---
+++ dashboard-widget-wp-2024.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Dashboard Widget 2024
+ * Description: Add a custom dashboard widget in WordPress Admin
+ * Version: 1.0
+ * Author: Edson Vieira Demetrio
+ */
+
+ if( ! defined('ABSPATH')){
+    exit;
+ }
